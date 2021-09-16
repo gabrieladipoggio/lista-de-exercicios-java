a921fca738ccbdbab9ff0e78f0ecd48fda977cb1
gabrieladipoggio <gabrieladipoggio@gmail.com>
2021-09-15 23:09:51 -0300

    Adiciona exercício 03 - Interfaces

--- 05_interfaces/ex03_rg/rg/src/Main.java
+++ 05_interfaces/ex03_rg/rg/src/Main.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Gabriela", 123456);
+        Pessoa p2 = new Pessoa("João", 123456);
+
+
+        // Sem Override imprime "É diferente", sobrescrevendo com Override é igual.
+        if (p1.equals(p2)){
+            System.out.println("É igual");
+        } else {
+            System.out.println("É diferente");
+        }
+    }
 }
--- 05_interfaces/ex03_rg/rg/src/Pessoa.java
+++ 05_interfaces/ex03_rg/rg/src/Pessoa.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Pessoa {
-}
+public class Pessoa {
+    private String nome;
+    private int rg;
+
+    public Pessoa(String nome, int rg) {
+        this.nome = nome;
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return rg == pessoa.rg;
+    }
+
+};
+
